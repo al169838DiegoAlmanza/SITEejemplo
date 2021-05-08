c85c292266c2e1c475725beb495e710ff601e344
al169838DiegoAlmanza <48567250+al169838DiegoAlmanza@users.noreply.github.com>
2021-05-07 18:14:42 -0600

    Agregar archivos de proyecto.

---
+++ SITE/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace SITE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pnl1.Controls.Add(this.label1);
+            this.pnl1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.pnl1.Location = new System.Drawing.Point(-2, -2);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(214, 453);
+            this.pnl1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label1.Location = new System.Drawing.Point(63, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SITE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
