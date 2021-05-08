de14124cd16ae6fcdd08952fff643ee591bb4361
TANYA MICHELLE RINCON TARANGO <al169870@alumnos.uacj.mx>
2021-05-08 07:50:03 -0600

    Pantalla historial

---
+++ SITE/Historial.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Demo
+{
+    partial class Historial
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Historial));
+            this.PanelMenuLateral = new System.Windows.Forms.Panel();
+            this.PicLogo = new System.Windows.Forms.PictureBox();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.BtnProveedores = new System.Windows.Forms.Button();
+            this.BtnHistorial = new System.Windows.Forms.Button();
+            this.BtnProductos = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.PanelEncabezado = new System.Windows.Forms.Panel();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.PanelMenuLateral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).BeginInit();
+            this.PanelEncabezado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelMenuLateral
+            // 
+            this.PanelMenuLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(54)))), ((int)(((byte)(79)))));
+            this.PanelMenuLateral.Controls.Add(this.PicLogo);
+            this.PanelMenuLateral.Controls.Add(this.BtnSalir);
+            this.PanelMenuLateral.Controls.Add(this.BtnProveedores);
+            this.PanelMenuLateral.Controls.Add(this.BtnHistorial);
+            this.PanelMenuLateral.Controls.Add(this.BtnProductos);
+            this.PanelMenuLateral.Controls.Add(this.btnVentas);
+            this.PanelMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenuLateral.Name = "PanelMenuLateral";
+            this.PanelMenuLateral.Size = new System.Drawing.Size(200, 743);
+            this.PanelMenuLateral.TabIndex = 0;
+            // 
+            // PicLogo
+            // 
+            this.PicLogo.ErrorImage = ((System.Drawing.Image)(resources.GetObject("PicLogo.ErrorImage")));
+            this.PicLogo.Image = ((System.Drawing.Image)(resources.GetObject("PicLogo.Image")));
+            this.PicLogo.Location = new System.Drawing.Point(4, 22);
+            this.PicLogo.Name = "PicLogo";
+            this.PicLogo.Size = new System.Drawing.Size(190, 190);
+            this.PicLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicLogo.TabIndex = 6;
+            this.PicLogo.TabStop = false;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnSalir.FlatAppearance.BorderSize = 0;
+            this.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSalir.Font = new System.Drawing.Font("AR ADGothicJP Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnSalir.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
+            this.BtnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnSalir.Location = new System.Drawing.Point(49, 629);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(101, 50);
+            this.BtnSalir.TabIndex = 5;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            // 
+            // BtnProveedores
+            // 
+            this.BtnProveedores.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnProveedores.FlatAppearance.BorderSize = 0;
+            this.BtnProveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnProveedores.Font = new System.Drawing.Font("AR ADGothicJP Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnProveedores.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnProveedores.Image = ((System.Drawing.Image)(resources.GetObject("BtnProveedores.Image")));
+            this.BtnProveedores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnProveedores.Location = new System.Drawing.Point(28, 324);
+            this.BtnProveedores.Name = "BtnProveedores";
+            this.BtnProveedores.Size = new System.Drawing.Size(156, 50);
+            this.BtnProveedores.TabIndex = 3;
+            this.BtnProveedores.Text = "Proveedores";
+            this.BtnProveedores.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnProveedores.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnProveedores.UseVisualStyleBackColor = true;
+            // 
+            // BtnHistorial
+            // 
+            this.BtnHistorial.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnHistorial.FlatAppearance.BorderSize = 0;
+            this.BtnHistorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHistorial.Font = new System.Drawing.Font("AR ADGothicJP Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnHistorial.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnHistorial.Image = ((System.Drawing.Image)(resources.GetObject("BtnHistorial.Image")));
+            this.BtnHistorial.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnHistorial.Location = new System.Drawing.Point(25, 486);
+            this.BtnHistorial.Name = "BtnHistorial";
+            this.BtnHistorial.Size = new System.Drawing.Size(143, 50);
+            this.BtnHistorial.TabIndex = 2;
+            this.BtnHistorial.Text = "Historial";
+            this.BtnHistorial.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnHistorial.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnHistorial.UseVisualStyleBackColor = true;
+            // 
+            // BtnProductos
+            // 
+            this.BtnProductos.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnProductos.FlatAppearance.BorderSize = 0;
+            this.BtnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnProductos.Font = new System.Drawing.Font("AR ADGothicJP Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnProductos.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnProductos.Image = ((System.Drawing.Image)(resources.GetObject("BtnProductos.Image")));
+            this.BtnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnProductos.Location = new System.Drawing.Point(28, 407);
+            this.BtnProductos.Name = "BtnProductos";
+            this.BtnProductos.Size = new System.Drawing.Size(143, 50);
+            this.BtnProductos.TabIndex = 1;
+            this.BtnProductos.Text = "Productos";
+            this.BtnProductos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnProductos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnVentas.FlatAppearance.BorderSize = 0;
+            this.btnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentas.Font = new System.Drawing.Font("AR ADGothicJP Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnVentas.ForeColor = System.Drawing.Color.Transparent;
+            this.btnVentas.Image = ((System.Drawing.Image)(resources.GetObject("btnVentas.Image")));
+            this.btnVentas.Location = new System.Drawing.Point(25, 245);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(143, 50);
+            this.btnVentas.TabIndex = 0;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // PanelEncabezado
+            // 
+            this.PanelEncabezado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(54)))), ((int)(((byte)(79)))));
+            this.PanelEncabezado.Controls.Add(this.Titulo);
+            this.PanelEncabezado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelEncabezado.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PanelEncabezado.Location = new System.Drawing.Point(200, 0);
+            this.PanelEncabezado.Name = "PanelEncabezado";
+            this.PanelEncabezado.Size = new System.Drawing.Size(1139, 112);
+            this.PanelEncabezado.TabIndex = 1;
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("AR ADGothicJP Medium", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Titulo.ForeColor = System.Drawing.Color.White;
+            this.Titulo.Location = new System.Drawing.Point(375, 37);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(416, 40);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "Historial de ventas";
+            this.Titulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Titulo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Historial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1339, 743);
+            this.Controls.Add(this.PanelEncabezado);
+            this.Controls.Add(this.PanelMenuLateral);
+            this.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Name = "Historial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SITE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelMenuLateral.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).EndInit();
+            this.PanelEncabezado.ResumeLayout(false);
+            this.PanelEncabezado.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMenuLateral;
+        private System.Windows.Forms.Panel PanelEncabezado;
+        private System.Windows.Forms.Button BtnProveedores;
+        private System.Windows.Forms.Button BtnHistorial;
+        private System.Windows.Forms.Button BtnProductos;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.PictureBox PicLogo;
+    }
+}
+
